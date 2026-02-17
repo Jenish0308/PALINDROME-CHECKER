8b177a245db9256ff97f6f1e1da24710a66cd220
Jenish0308 <your-email@gmail.com>
2026-02-17 16:05:02 +0530

    UC5:stack based palindrome checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,22 +1,25 @@
+import java.util.Stack;
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String inputText = "radar";   // hardcoded input
-        char[] characters = inputText.toCharArray();
+        String inputText = "noon";   // hardcoded input
+        Stack<Character> stack = new Stack<>();
 
-        boolean isPalindrome = true;
-        int left = 0;
-        int right = characters.length - 1;
-
-        while (left < right) {
-            if (characters[left] != characters[right]) {
-                isPalindrome = false;
-                break;
-            }
-            left++;
-            right--;
+        // Push characters into stack
+        for (int i = 0; i < inputText.length(); i++) {
+            stack.push(inputText.charAt(i));
         }
+
+        String reversedText = "";
+
+        // Pop characters to form reversed string
+        while (!stack.isEmpty()) {
+            reversedText = reversedText + stack.pop();
+        }
+
+        boolean isPalindrome = inputText.equals(reversedText);
 
         System.out.println("Input Text : " + inputText);
         System.out.println("Is it a Palindrome? : " + isPalindrome);
