e3e05ce03f6bcebdc72d741cbfb9ab8c3714ef4d
Jenish0308 <your-email@gmail.com>
2026-02-17 14:56:01 +0530

    Base application setup

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Palindrome Checker App");
+        System.out.println("=================================");
+        System.out.println("  Welcome to Palindrome Checker  ");
+        System.out.println("=================================");
     }
 }
-
